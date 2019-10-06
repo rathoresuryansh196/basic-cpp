3dcb446673b1de7497b96abe7878cf3d4fedd599
SURYANSH SINGH RATHORE <rathoresuryansh196@gmail.com>
2019-10-06 20:37:03 +0530

    Update a.cpp
    adding work comments

--- struct/a.cpp
+++ struct/a.cpp
@@ -11,18 +11,22 @@ int main()
         int qty;
     } product1, product2;
 
+    //initialize product1
     product1.name = "Milo";
     product1.price = 33.99;
     product1.qty = 50;
 
+    //output product1
     cout << product1.name << endl
          << product1.price << endl
          << product1.qty << endl;
 
+    //initialize product2
     product2.name = "Horlic";
     product2.price = 23.99;
     product2.qty = 25;
 
+    //output product2
     cout << product2.name << endl
          << product2.price << endl
          << product2.qty << endl;
